31f85f3a577fba6f1132ecc15f06b978ecae2afa
szwojtkowski <wojtkowski.sz@gmail.com>
2016-04-24 17:10:18 +0200

    hello world

---
+++ app/src/androidTest/java/mcc/agh/edu/pl/sandbox/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mcc.agh.edu.pl.sandbox;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
