c31851bbeff9b4e1ce24a974d222793737359be8
szwojtkowski <wojtkowski.sz@gmail.com>
2017-01-17 02:51:21 +0100

    removed unnecessary test cases

--- app/src/main/java/mcc/agh/edu/pl/tests/TestLauncher.java
+++ app/src/main/java/mcc/agh/edu/pl/tests/TestLauncher.java
@@ -15,30 +15,7 @@ import mcc.agh.edu.pl.mobilecloudcomputinglibrary.service.SmartOffloadingService
 public class TestLauncher {
 
     private String[] tests = new String[]{
-            "ArraySumTask,1;2;3;4;5;6;7;8;9;10;11;19;22;13;13;12",
-    //        "ArraySumTask,12;22;23;24;25;26;27;28;29",
-            "ArraySumTask,11;21;31;4;15;16;71;8;91;110;11",
-            "ArraySumTask,31;32;33;34;53;63;73;38;39;103;113;33;999;11;1;2;3;4;5;6;7;8;9;10",
-            "QuickSortTask,1;2;3;4;5;6;7;8;9;10;11;0;8;8",
-            "QuickSortTask,12;22;23;24;25;26;27;28;29;210;211",
-    //        "QuickSortTask,11;21;31;4;15;16;71;8",
-            "QuickSortTask,31;32;33;34;53;63;73;38;39;103;113;1;2;3;4;22;14",
-            "SimpleOCRTask,/storage/emulated/0/Documents/andromed.png",
-    //        "SimpleOCRTask,/storage/emulated/0/Documents/barak.png",
-    //        "SimpleOCRTask,/storage/emulated/0/Documents/bh.png",
-            "SimpleOCRTask,/storage/emulated/0/Documents/bht.png",
-            "SimpleOCRTask,/storage/emulated/0/Documents/davis.png",
-    //        "SimpleOCRTask,/storage/emulated/0/Documents/inwaz.png",
-    //        "SimpleOCRTask,/storage/emulated/0/Documents/kans.png",
-    //        "SimpleOCRTask,/storage/emulated/0/Documents/kansas.png",
-    //        "SimpleOCRTask,/storage/emulated/0/Documents/nasa.png",
-            "SimpleOCRTask,/storage/emulated/0/Documents/ncrp.png",
-            "SimpleOCRTask,/storage/emulated/0/Documents/procinw.png",
-            "SimpleOCRTask,/storage/emulated/0/Documents/rak.png",
-    //        "SimpleOCRTask,/storage/emulated/0/Documents/rewkub.png",
-            "SimpleOCRTask,/storage/emulated/0/Documents/rtsz.png",
-            "SimpleOCRTask,/storage/emulated/0/Documents/wjntrz.png",
-            "SimpleOCRTask,/storage/emulated/0/Documents/zebra.png"
+            "ArraySumTask,1;2;3;4;5;6;7;8;9;10;11;19;22;13;13;12"
     };
 
     private TestCaseFactory factory;
